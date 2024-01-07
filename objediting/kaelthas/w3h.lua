1c49bc7ae3710f131e4dfaecb74ab840448de027
Halflight <halflumi@outlook.com>
2024-01-07 21:25:00 +0800

    Rain of Hellfire

--- objediting/kaelthas/w3h.lua
+++ objediting/kaelthas/w3h.lua
@@ -6,3 +6,20 @@ b:setTooltipNormal(1, "Floating Chaos")
 b:setTooltipNormalExtended(1,
     "This unit was hit by Floating Chaos."
 )
+
+b = BuffDefinition:new(BID.RAIN_OF_HELLFIRE_DOT, "BNrd")
+b:setIconNormal(1, RAIN_OF_HELLFIRE.ICON)
+b:setTooltipNormal(1, "Rain of Hellfire")
+b:setTooltipNormalExtended(1,
+    "This unit was hit by Rain of Hellfire; it will take damage over time."
+)
+
+b = BuffDefinition:new(BID.RAIN_OF_HELLFIRE_EFFECT, "XHbz")
+b:setEffect(1, RAIN_OF_HELLFIRE.MDX)
+b:setEffectSound(1, "")
+b:setEffectSoundLooping(1, "")
+
+b = BuffDefinition:new(BID.RAIN_OF_HELLFIRE_SOUND_EFFECT, "XHbz")
+b:setEffect(1, "")
+b:setEffectSound(1, "RainOfFireWave")
+b:setEffectSoundLooping(1, "")
--- objediting/kaelthas/w3s.lua
+++ objediting/kaelthas/w3s.lua
@@ -122,3 +122,60 @@ for i = 1, FLOATING_CHAOS.MAX_LEVEL do
     a:setPrimaryDamage(i, FLOATING_CHAOS.DMG(i) / 2)
     a:setTargetsAllowed(i, [[ground,enemy,neutral,air,structure]])
 end
+
+
+-- Rain of Hellfire
+a = AbilityDefinitionIllidanChannel:new(AID.RAIN_OF_HELLFIRE)
+a:setLevels(RAIN_OF_HELLFIRE.MAX_LEVEL)
+a:setLevelSkipRequirement(6)
+a:setArtCaster("")
+a:setArtEffect("")
+a:setArtTarget("")
+a:setButtonPositionNormalX(3)
+a:setButtonPositionNormalY(2)
+a:setButtonPositionResearchX(3)
+a:setHotkeyLearn("R")
+a:setHotkeyNormal("R")
+a:setIconNormal(RAIN_OF_HELLFIRE.ICON)
+a:setIconResearch(RAIN_OF_HELLFIRE.ICON)
+a:setToolTips(RAIN_OF_HELLFIRE.TOOLTIPS, RAIN_OF_HELLFIRE.MAX_LEVEL)
+for i = 1, RAIN_OF_HELLFIRE.MAX_LEVEL do
+    a:setAreaofEffect(i, RAIN_OF_HELLFIRE.AOE)
+    a:setBaseOrderID(i, "charm")
+    a:setCastRange(i, RAIN_OF_HELLFIRE.CAST_RANGE)
+    a:setCooldown(i, RAIN_OF_HELLFIRE.CD)
+    a:setDisableOtherAbilities(i, false)
+    a:setDurationNormal(i, RAIN_OF_HELLFIRE.INTERVAL) -- For tooltips/triggers.
+    a:setDurationHero(i, RAIN_OF_HELLFIRE.INTERVAL_SOUND) -- For tooltips/triggers.
+    a:setFollowThroughTime(i, RAIN_OF_HELLFIRE.DUR)
+    a:setManaCost(i, RAIN_OF_HELLFIRE.COST)
+    a:setOptions(i, 3)
+    a:setTargetType(i, 2)
+end
+
+a = AbilityDefinitionRainofFire:new(AID.RAIN_OF_HELLFIRE_IMPL)
+a:setLevels(RAIN_OF_HELLFIRE.MAX_LEVEL)
+for i = 1, RAIN_OF_HELLFIRE.MAX_LEVEL do
+    a:setBuffs(i, CommaString(BID.RAIN_OF_HELLFIRE_DOT, "BNrf"))
+    a:setAreaofEffect(i, RAIN_OF_HELLFIRE.SHARD_AOE)
+    a:setBuildingReduction(i, 1.0)
+    a:setCastRange(i, 1000000000.0)
+    a:setCooldown(i, 0.0)
+    a:setDamage(i, RAIN_OF_HELLFIRE.DMG(i))
+    a:setDamagePerSecond(i, RAIN_OF_HELLFIRE.DOT_DMG(i))
+    a:setDurationHero(i, RAIN_OF_HELLFIRE.DOT_DUR)
+    a:setDurationNormal(i, RAIN_OF_HELLFIRE.DOT_DUR)
+    a:setEffects(i, BID.RAIN_OF_HELLFIRE_EFFECT)
+    a:setMaximumDamageperWave(i, 0)
+    a:setNumberofShards(i, 1)
+    a:setNumberofWaves(i, 1)
+    a:setTargetsAllowed(i, "enemy")
+end
+
+a = AbilityDefinitionArchMageBlizzard:new(AID.RAIN_OF_HELLFIRE_SOUND)
+a:setAreaofEffect(1, 0)
+a:setCastRange(1, 1000000000.0)
+a:setCooldown(1, 0.0)
+a:setEffects(1, BID.RAIN_OF_HELLFIRE_SOUND_EFFECT)
+a:setNumberofWaves(1, 1)
+a:setTargetsAllowed(1, "self")
