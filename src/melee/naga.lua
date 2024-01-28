4e1f063c2c4d766ac5e726e3b64f5141857e808c
Halflight <halflumi@outlook.com>
2024-01-28 14:36:05 +0800

    remote

--- src/melee/naga.lua
+++ src/melee/naga.lua
@@ -1,7 +1,10 @@
 OnInit.map(function()
     for i = 0, bj_MAX_PLAYERS - 1 do
-        if (GetPlayerController(Player(i)) == MAP_CONTROL_USER) then
-            
+        local p = Player(i)
+        if (GetPlayerController(p) == MAP_CONTROL_USER) then
+            if (IsPlayerRacePrefSet(p, RACE_PREF_RANDOM)) then
+
+            end
         end
     end
 end)
